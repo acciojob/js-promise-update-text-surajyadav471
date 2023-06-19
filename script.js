d743e167d464e5cbedf4406addc14bb61248bb6c
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-06-19 20:06:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function updateTextAfterDelay() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+// Usage example
+const outputElement = document.getElementById('output');
+
+updateTextAfterDelay()
+  .then((result) => {
+    outputElement.textContent = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
